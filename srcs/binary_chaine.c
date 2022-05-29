d6becf74af7c6e65493cab0c7c8856babf476fc0
awallet <awallet@student.42.fr>
2022-05-29 20:49:03 +0200

    Fix readChaine et writeChaine

--- srcs/binary_chaine.c
+++ srcs/binary_chaine.c
@@ -6,7 +6,7 @@
 /*   By: awallet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 18:17:17 by awallet           #+#    #+#             */
-/*   Updated: 2022/05/29 19:44:25 by awallet          ###   ########.fr       */
+/*   Updated: 2022/05/29 20:48:14 by awallet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	ft_wchaine(struct s_chaineoctet *self, char *string)
 		ft_wshort(self, length);
 		self->len += length;
 		while (*string)
-			self->buffer[self->pos++] = string++;
+			self->buffer[self->pos++] = *string++;
 	}
 }
 //! Amélioration nécessaire :)
@@ -34,9 +34,7 @@ unsigned char	*ft_rchaine(struct s_chaineoctet *self)
 	unsigned int	i;
 
 	length = ft_rshort(self);
-	printf("\n taille: %i", length);
-	i = -1;
-	while (++i < length)
+	while (length--)
 		ft_putchar_fd(self->buffer[self->pos++], 1);
 	return ("");
 }
